d79dadf498af45698f67f1e7bffe3f3e0af4d510
xgenya <29149596+xgenya@users.noreply.github.com>
2024-05-20 18:05:09 +0800

    Integrated material UI
    Signed-off-by: xgenya <29149596+xgenya@users.noreply.github.com>

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,21 +2,34 @@
  * @Author: Rem
  * @Date: 2024-05-20 16:54:44
  * @LastEditors: Rem
- * @LastEditTime: 2024-05-20 17:19:27
+ * @LastEditTime: 2024-05-20 17:58:50
  * @FilePath: /weight-ui/src/renderer/App.tsx
- * @FileDescription:
- * @Description:
+ * @FileDescription: App.tsx
+ * @Description:  Main entry file
  * Do one thing at a time, and do well.
  * Copyright (c) 2024 by Rem, All Rights Reserved.
  */
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import 'tailwindcss/tailwind.css';
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import { Button } from '@mui/material';
+
+function handleClick() {
+  console.log('Hello World');
+}
 
 function Main() {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center h-screen space-y-4">
       <h1 className="text-4xl text-center">Hello, Electron!</h1>
+      <Button variant="contained" onClick={handleClick}>
+        Hello World
+      </Button>
     </div>
   );
 }
